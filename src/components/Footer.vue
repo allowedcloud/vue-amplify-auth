8de9e0290a9a8ac10b5bebf9eaa16e7e2fa92c0d
AllowedCloud <79382276+allowedcloud@users.noreply.github.com>
2021-09-21 21:19:38 -0400

    Remove more template stuff

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,26 +11,20 @@ const toggleLocales = () => {
 </script>
 
 <template>
-  <nav class="text-xl mt-6">
-    <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <carbon-campsite />
-    </router-link>
+  <router-link to="/" :title="t('button.home')">
+    <carbon-campsite />
+  </router-link>
 
-    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
-      <carbon-moon v-if="isDark" />
-      <carbon-sun v-else />
-    </button>
+  <button :title="t('button.toggle_dark')" @click="toggleDark()">
+    <carbon-moon v-if="isDark" />
+    <carbon-sun v-else />
+  </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <carbon-language />
-    </a>
+  <a :title="t('button.toggle_langs')" @click="toggleLocales">
+    <carbon-language />
+  </a>
 
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <carbon-dicom-overlay />
-    </router-link>
-
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
-      <carbon-logo-github />
-    </a>
-  </nav>
+  <router-link to="/about" :title="t('button.about')">
+    <carbon-dicom-overlay />
+  </router-link>
 </template>
