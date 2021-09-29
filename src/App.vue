bb43178c4de7b604094ea345a8f0099751020abe
AllowedCloud <79382276+allowedcloud@users.noreply.github.com>
2021-09-29 11:58:25 -0400

    Update title

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Vue Amplify Auth',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'User auth in Vue 3 with AWS Amplify' },
   ],
 })
 </script>
