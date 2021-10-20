beac9d85556e1980c5b98ef5a8952950dcec63f7
AllowedCloud <79382276+allowedcloud@users.noreply.github.com>
2021-10-20 11:18:36 -0400

    Add title tag to index.html for w3c validation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { useStore } from './stores/auth'
-const store = useStore()
-store.load()
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
